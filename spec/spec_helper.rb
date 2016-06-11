6ff969ca2543e9e4e4dcf2efcb91730d571a207f
Sebastián Katzer <katzer.sebastian@googlemail.com>
2016-06-11 17:22:50 +0200

    filter out lib/extensions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ CodeClimate::TestReporter.start
 
 SimpleCov.start do
   add_filter '/spec'
+  add_filter '/lib/extensions'
   formatter SimpleCov::Formatter::MultiFormatter.new(
     [
       SimpleCov::Formatter::HTMLFormatter,
