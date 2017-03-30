ada23a42966a5891cbc35eef8ef9b2ba1dc7dd4e
Sebastián Katzer <katzer.sebastian@googlemail.com>
2017-03-30 10:29:05 +0200

    Fix codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,13 @@
 require 'simplecov'
 require 'webmock/rspec'
-require 'codeclimate-test-reporter'
 require 'timecop'
 require 'pry'
 
 WebMock.disable_net_connect!(allow: 'codeclimate.com')
 
-CodeClimate::TestReporter.start
-
 SimpleCov.start do
   add_filter '/spec'
   add_filter '/lib/extensions'
-  formatter SimpleCov::Formatter::MultiFormatter.new(
-    [
-      SimpleCov::Formatter::HTMLFormatter,
-      CodeClimate::TestReporter::Formatter
-    ]
-  )
 end
 
 Dir['lib/**/*.rb'].each { |f| require_relative "../#{f}" }
